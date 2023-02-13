27484c525a2f9d0e66fb2dae59a05a10b08070cb
Khalid Sadat <khalid_saadat@hotmail.com>
2023-02-12 20:16:19 -0500

    Added the base strucutre to connect frontend with backend. #25

--- Linkify-src/server/server.js
+++ Linkify-src/server/server.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const connectDB = require("./config/connectDB.js");
-const userRouter = require("./routes/accountRoutes");
-const cvRouter = require("./routes/cvRoutes");
-const userPropertyRouter = require("./routes/userPropertyRoutes");
-const connectionRoutes = require("./routes/connectionRoutes");
+const userRouter = require("./routes/accountR");
+const cvRouter = require("./routes/cvR");
+const userPropertyRouter = require("./routes/userPropertyR");
+const connectionRoutes = require("./routes/connectionR");
 const dotenv = require("dotenv");
 const app = express();
 
