0544df26156bb033b3238f807946ef01e626f2ee
Khalid Sadat <khalid_saadat@hotmail.com>
2023-03-11 21:20:20 -0500

    Added test cases for sprint 3: #6
    Fixed merge conflicts again.

--- Linkify-src/server/routes/userPropertyR.js
+++ Linkify-src/server/routes/userPropertyR.js
@@ -7,27 +7,12 @@ router.post("/addSkill", userPropertyC.addSkill);
 router.delete("/deleteSkill", userPropertyC.deleteSkill);
 router.post("/addLanguage", userPropertyC.addLanguage);
 router.delete("/deleteLanguage", userPropertyC.deleteLanguage);
-
-router.delete("/deleteProject", userPropertyC.deleteProject);
-
-// @desc Adding a project
-// @route POST /api/user/property/addProject
-// @access Public
 router.post("/addProject", userPropertyC.addProject);
-
-// @desc Add new experience to user profile
-// @route POST /api/user/property/addExperience
-// @access Public
+router.delete("/deleteProject", userPropertyC.deleteProject);
 router.post("/addExperience", userPropertyC.editExperience);
-
 router.delete("/deleteExperience", userPropertyC.deleteExperience);
 router.post("/addEducation", userPropertyC.addEducation);
 router.delete("/deleteEducation", userPropertyC.deleteEducation);
 router.post("/addLocation", userPropertyC.addLocation);
-
-// @desc Edit an existing education from user profile
-// @route PUT /api/user/property/editEducation
-// @access Public
-// router.put("/editEducation", userPropertyC.editEducation);
 
 module.exports = router;
