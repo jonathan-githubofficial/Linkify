fad23e68fe4475a874ccea7168d4122cd5fa2048
Khalid Sadat <khalid_saadat@hotmail.com>
2023-03-15 23:58:27 -0400

    Trying to restrict users from seeing the navbar if not logged in.

--- Linkify-src/client/src/components/shared/Navbar.jsx
+++ Linkify-src/client/src/components/shared/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 
 import profile_pic from "../../static/images/profile.jpg";
@@ -8,6 +8,13 @@ import logo from "../../static/images/logo.svg";
 
 function Navbar() {
   const navigate = useNavigate();
+
+  const [isLogged, setIsLogged] = useState(1);
+
+  if(localStorage.getItem('loggedIn') == 1) {
+    setIsLogged(2);
+  }
+
   const logout = () => {
     localStorage.removeItem("uid");
     localStorage.removeItem("loggedIn");
@@ -23,87 +30,94 @@ function Navbar() {
           </Link>
         </div>
       </div>
-      <div className="navbar-center hidden lg:block">
-        <div className="form-control w-[47rem]">
-          <input
-            type="text"
-            placeholder="Search Linkify..."
-            className="input input-bordered"
-          />
-        </div>
-      </div>
-      <div className="navbar-end">
-        <Link to="/messages">
-          <div
-            className="mr-2 btn btn-ghost btn-circle"
-            style={{ fontSize: "22px" }}
-          >
-            <FaComments />
-          </div>
-        </Link>
-        <Link to="/jobs">
-          <div
-            className="mr-2 btn btn-ghost btn-circle"
-            style={{ fontSize: "20px" }}
-          >
-            <FaSuitcase />
-          </div>
-        </Link>
 
-        <Link to="/notifications">
-          <div className="dropdown dropdown-end mr-5">
-            <label tabIndex={0} className="btn btn-ghost btn-circle">
-              <div className="indicator">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke-width="1.5"
-                  stroke="currentColor"
-                  class="w-6 h-6"
-                >
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0"
-                  />
-                </svg>
-                {/* <span className="badge badge-sm indicator-item">6</span> */}
+      {(isLogged && isLogged == 2) && 
+        <div className="navbar-center hidden lg:block">
+          <div className="form-control w-[47rem]">
+            <input
+              type="text"
+              placeholder="Search Linkify..."
+              className="input input-bordered"
+            />
+          </div>
+        </div>
+      }
+
+      {isLogged == 2 && 
+        <div className="navbar-end">
+          <Link to="/messages">
+            <div
+              className="mr-2 btn btn-ghost btn-circle"
+              style={{ fontSize: "22px" }}
+            >
+              <FaComments />
+            </div>
+          </Link>
+          <Link to="/jobs">
+            <div
+              className="mr-2 btn btn-ghost btn-circle"
+              style={{ fontSize: "20px" }}
+            >
+              <FaSuitcase />
+            </div>
+          </Link>
+
+          <Link to="/notifications">
+            <div className="dropdown dropdown-end mr-5">
+              <label tabIndex={0} className="btn btn-ghost btn-circle">
+                <div className="indicator">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke-width="1.5"
+                    stroke="currentColor"
+                    class="w-6 h-6"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0"
+                    />
+                  </svg>
+                  {/* <span className="badge badge-sm indicator-item">6</span> */}
+                </div>
+              </label>
+              <div
+                tabIndex={0}
+                className="mt-3 card card-compact dropdown-content w-52 bg-base-100 shadow"
+              ></div>
+            </div>
+          </Link>
+
+          <div className="dropdown dropdown-end">
+            <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+              <div className="w-10 rounded-full">
+                <img src={profile_pic} />
               </div>
             </label>
-            <div
+            <ul
               tabIndex={0}
-              className="mt-3 card card-compact dropdown-content w-52 bg-base-100 shadow"
-            ></div>
+              className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+            >
+              <Link to="/profile">
+                <li>
+                  <a className="justify-between">Profile</a>
+                </li>
+              </Link>
+              <li>
+                <a>Settings</a>
+              </li>
+              <p onClick={logout}>
+                <li>
+                  <a>Logout</a>
+                </li>
+              </p>
+            </ul>
           </div>
-        </Link>
-
-        <div className="dropdown dropdown-end">
-          <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
-            <div className="w-10 rounded-full">
-              <img src={profile_pic} />
-            </div>
-          </label>
-          <ul
-            tabIndex={0}
-            className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
-          >
-            <Link to="/profile">
-              <li>
-                <a className="justify-between">Profile</a>
-              </li>
-            </Link>
-            <li>
-              <a>Settings</a>
-            </li>
-            <p onClick={logout}>
-              <li>
-                <a>Logout</a>
-              </li>
-            </p>
-          </ul>
         </div>
-      </div>
+      }
+
     </div>
   );
 }
