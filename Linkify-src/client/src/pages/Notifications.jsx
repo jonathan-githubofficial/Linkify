eb86fd347278b7a34143254586ccf5882e016988
daria-koroleva <dasha.koroleva499@gmail.com>
2023-03-26 13:28:24 -0400

    Move notification component to a new folder

--- Linkify-src/client/src/pages/Notifications.jsx
+++ Linkify-src/client/src/pages/Notifications.jsx
@@ -1,6 +1,6 @@
 import React, { useState , useEffect} from 'react';
 import { Helmet } from 'react-helmet';
-import Notification from '../components/Notification';
+import Notification from '../components/notifications/Notification';
 import { useNavigate } from "react-router-dom";
 
 function Notifications() {
@@ -18,7 +18,7 @@ function Notifications() {
     { id: 1, age:"16m", user: "Monkey", type: "posted", description: "I am a happy Developer. I have five years of experience. I use HTML, JS, and CSS. I am also learning React.", avatar: "/images/avatar1.jpg" },
     { id: 2, age:"1h", user: "Bunny", type: "commented", description: "What if every window in your house could generate electricity? Learn more about the importance of solar power", avatar: "/images/avatar2.jpg" },
     { id: 3, age:"5h",user: "Cow", type: "posted", description: "Our Leadership Masterclass Series is starting soon!Feel free to message me if you have any questions.", avatar: "/images/avatar3.jpg" },
-    { id: 4, age:"14h",user: "Monkey", type: "posted", description: "Passionate about Data Governance and Management? My team is growing and we are looking for Data Stewards to continuously bring value to our data consumers and producers.", avatar: "/images/avatar4.jpg" },
+    { id: 4, age:"14h",user: "Bear", type: "posted", description: "Passionate about Data Governance and Management? My team is growing and we are looking for Data Stewards to continuously bring value to our data consumers and producers.", avatar: "/images/avatar4.jpg" },
     { id: 5, age:"1d",user: "Bear", type: "commented", description: "We are honoured to be included in this year’s #SustainableYearbook as one of the top 5% sustainability performers in the transportation industry.", avatar: "/images/avatar5.jpg" },
     { id: 6, age:"5d",user: "Fox", type: "posted", description: "Hiring a Front-End Developer with experience in JS, HTML and CSS. Knowing React and UX design will be a plus. Remote Opportunity and competitive compensation.", avatar: "/images/avatar6.jpg" }
    
