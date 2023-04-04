052a4ff27f6206daa4f85657f8d9db3d8b2f0bed
Khalid Sadat <khalid_saadat@hotmail.com>
2023-04-03 20:24:21 -0400

    Added the base structure files for each unit test: #6

---
+++ Linkify-src/server/test/message.js
@@ -0,0 +1,3 @@
+// Messages base test file
+// It is being used as the base strucutre for our unit test.
+// It is left empty intentionally because it is required by Chai and Mocha's unit testing structure.
