74ba1e6527da6e13666e7d4c57365e9545c6c11b
Khalid Sadat <khalid_saadat@hotmail.com>
2023-04-07 11:51:03 -0400

    Added breadcrumbs in events and groups

--- Linkify-src/client/src/components/groups/CreateGroup.jsx
+++ Linkify-src/client/src/components/groups/CreateGroup.jsx
@@ -87,8 +87,36 @@ export default function CreateGroup() {
                     <Sidebar name='Khalid Sadat' />
 
                     <div className="w-full w-full lg:w-3/4 bg-white relative lg:rounded-t-xl p-5">
+
+                        <nav className="flex mb-10 ml-12 mt-3" aria-label="Breadcrumb">
+                            <ol className="inline-flex items-center space-x-1 md:space-x-3">
+                                <li className="inline-flex items-center">
+                                    <Link to="/" className="inline-flex items-center text-sm font-medium text-gray-700 hover:text-blue-600 dark:text-gray-400 dark:hover:text-white">
+                                        <svg aria-hidden="true" className="w-4 h-4 mr-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z" /></svg>
+                                        Home
+                                    </Link>
+                                    </li>
+                                <li>
+                                    <div className="flex items-center">
+                                        <svg aria-hidden="true" className="w-6 h-6 text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clipRule="evenodd" /></svg>
+                                        <Link to="/groups" className="ml-1 text-sm font-medium text-gray-700 hover:text-blue-600 md:ml-2 dark:text-gray-400 dark:hover:text-white">
+                                            Groups
+                                        </Link>
+                                    </div>
+                                </li>
+                                <li aria-current="page">
+                                    <div className="flex items-center">
+                                        <svg aria-hidden="true" className="w-6 h-6 text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clipRule="evenodd" /></svg>
+                                        <span className="ml-1 text-sm font-medium text-gray-500 md:ml-2 dark:text-gray-400">
+                                            Create Group
+                                        </span>
+                                    </div>
+                                </li>
+                            </ol>
+                        </nav>
+
                         <div className="flex flex-col justify-between gap-3">
-                            <h2 className="flex justify-center text-2xl font-bold md:text-3xl m-12">Create a new group</h2>
+                            <h2 className="flex justify-center text-2xl font-bold md:text-3xl">Create a new group</h2>
                         </div>
 
                         {isCreated == true && 
@@ -97,36 +125,38 @@ export default function CreateGroup() {
                             </div>
                         }
 
-                        <form>
-                            <div className="mb-6">
-                                <label htmlFor="group-name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Group Name</label>
-                                <input type="text" id="group-name" value={name} onChange={handleGroupName} className=" border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Enter your group name" required />
-                            </div>
-                            <div className="mb-6">
-                                <label htmlFor="description" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Description</label>
-                                <textarea id="description" value={description} onChange={handleDescription} rows={4} className="block p-2.5 w-full text-sm text-gray-900  rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Describe your group..." defaultValue={""} />
-                            </div>
-                            <div className="flex mb-6">
-                                <div className="flex items-center h-5">
-                                    <input id="status-active" onChange={handleStatus} checked={status} aria-describedby="status-active-text" type="checkbox" defaultValue className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
+                        <div className="m-12">
+                            <form>
+                                <div className="mb-6">
+                                    <label htmlFor="group-name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Group Name</label>
+                                    <input type="text" id="group-name" value={name} onChange={handleGroupName} className=" border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Enter your group name" required />
                                 </div>
-                                <div className="ml-2 text-sm">
-                                    <label htmlFor="status-active" className="font-medium text-gray-900 dark:text-gray-300">Active</label>
-                                    <p id="status-active-text" className="text-xs font-normal text-gray-500 dark:text-gray-300">
-                                        Active Status: This group will be active and users can join it anytime.
-                                    </p>
+                                <div className="mb-6">
+                                    <label htmlFor="description" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Description</label>
+                                    <textarea id="description" value={description} onChange={handleDescription} rows={4} className="block p-2.5 w-full text-sm text-gray-900  rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Describe your group..." defaultValue={""} />
                                 </div>
+                                <div className="flex mb-6">
+                                    <div className="flex items-center h-5">
+                                        <input id="status-active" onChange={handleStatus} checked={status} aria-describedby="status-active-text" type="checkbox" defaultValue className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
+                                    </div>
+                                    <div className="ml-2 text-sm">
+                                        <label htmlFor="status-active" className="font-medium text-gray-900 dark:text-gray-300">Active</label>
+                                        <p id="status-active-text" className="text-xs font-normal text-gray-500 dark:text-gray-300">
+                                            Active Status: This group will be active and users can join it anytime.
+                                        </p>
+                                    </div>
+                                </div>
+
+                                <button onClick={createGroup} className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                                    Create
+                                </button>
+                            </form>
+
+                            <hr className="mt-6 mb-6" />
+                            
+                            <div className="text-gray-500 text-sm">
+                                Note: You will be the owner of this group.
                             </div>
-
-                            <button onClick={createGroup} className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                                Create
-                            </button>
-                        </form>
-
-                        <hr className="mt-6 mb-6" />
-                        
-                        <div className="text-gray-500 text-sm">
-                            Note: You will be the owner of this group.
                         </div>
                     </div>
 
