9009d33723c23c3a6119b3a236c388a174227d96
iq882 <iq882@gmail.com>
2023-04-22 20:00:41 +0500

    deploy/cyclic: change server js code

--- server/server.js
+++ server/server.js
@@ -82,9 +82,9 @@ app.use(
 
 // __dirname = path.resolve();
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "./client/dist")));
+  app.use(express.static(path.join(__dirname, "../client/build")));
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "./client/dist/index.html"));
+    res.sendFile(path.resolve(__dirname, "../client/build/index.html"));
   });
 } else {
   app.get("/", (req, res) => {
